6dd987b0e9016fae3bc20931a5a638d66dbe354a
phantomhand <frank@phantomhand.com>
2013-12-18 03:13:04 -0500

    add admin panel controller and view

---
+++ views/v_admin.php
@@ -0,0 +1,15 @@
+<div class="panel panel-default">
+	<div class="panel-body">
+		<div class="container">	
+		
+		<h2>Admin Panel</h2>
+		<hr style="width: 94%; margin-left: -2px;">
+		
+		<h3><?php echo $user->first_name.'&nbsp;'.$user->last_name; ?></h3>
+		<br>
+		<h4>Actions</h4>
+		<p><a href="films/add">Add a film to the database</a></p>
+		
+		</div>
+	</div>
+</div>
