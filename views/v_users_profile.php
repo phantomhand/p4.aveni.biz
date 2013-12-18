b6466d39d697366d2419fd5e79c3a3e9922fa642
phantomhand <frank@phantomhand.com>
2013-12-18 03:17:17 -0500

    make profile two columns and adjust style

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -2,6 +2,24 @@
 	<div class="panel-body">
 		<div class="container">
 		
+		<div class="col-lg-6">
+			<h2><?=$user->first_name?>'s Profile</h2>	
+			<p>
+			<strong>Name:</strong> <?=$user->first_name?> <?=$user->last_name?><br>
+			
+			<strong>Email:</strong> <?=$user->email?><br>
+			
+			<form method='POST' action='/posts/p_add'>
+				<label for='content'><b>Add a new post:</b></label><br>
+				<textarea style="width: 400px; height: 80px;" name="content" id='content'></textarea>
+				<br>
+				<br>
+				<input type='submit' value='Add New Post' class="btn btn-primary" style="float: left; margin-top: -12px;">	
+			</form>	
+		</div>
+		
+		<br>
+		<div class="col-lg-6">
 			<?php if ( ($user->image == NULL) ): ?> 
 				<img class="avatar-lg" src="/images/avatar-question.jpg" alt="User Pic" height="200" width="200"/>
 			
@@ -12,26 +30,10 @@
 				<label for="content">Update your profile image</label>
 				<form method='POST' enctype="multipart/form-data" action='/users/add_image/'>
 					<input type='file' name='image'>
-					<br>
+
 					<input type='submit' value='Submit' class="btn btn-primary">
 				</form>
-		</figure>
-		
-		
-		<h2><?=$user->first_name?>'s Profile</h2>
-	
-		
-		<p>
-		<strong>Name:</strong> <?=$user->first_name?> <?=$user->last_name?><br>
-		
-		<strong>Email:</strong> <?=$user->email?><br>
-		
-		<form method='POST' action='/posts/p_add'>
-			<label for='content'><b>Add a new post:</b></label><br>
-			<textarea style="width: 400px; height: 80px;" name="content" id='content'></textarea>
-			
-			<input type='submit' value='Add New Post' class="btn btn-primary" style="float: left; margin-top: -12px;">	
-		</form>
+		</div>
 		
 		</div> <!-- /container -->
 	</div>
