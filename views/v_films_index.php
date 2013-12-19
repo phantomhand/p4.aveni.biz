f1a107e506a70cacb472de03cf9c0650f987b180
phantomhand <frank@phantomhand.com>
2013-12-19 17:39:11 -0500

    change film title to h4

--- views/v_films_index.php
+++ views/v_films_index.php
@@ -2,21 +2,20 @@
 	<div class="panel-body">
 		<div class="container ">		
 			<h2>DER Documentaries</h2>
-			<hr style="width: 94%; margin-left: -2px;">
+			<hr class="hr">
 			
 			<?php foreach($films as $film):?>					
-				<div> <!-- open individual film cell -->
-					<a href="/films/<?=$film['unique_id']?>"><img src="/images/<?=$film['image']?>" style="height: 75px; width: 100px; float: left; margin-right: 10px;"></a>
+				<div class="single col-lg-11"> <!-- open individual film cell -->
+					<a href="/films/<?=$film['unique_id']?>"><img class="film-sm" src="/images/<?=$film['image']?>"></a>
 						<!-- Show the film info and content -->
-						<h4><a href="/films/<?=$film['unique_id']?>"><?=$film['title']?><?php if(!empty($film['alt_title'])):?>: <?=$film['alt_title']?>
-							<?php endif; ?></a></h4>
+						<h4><a href="/films/id/<?=$film['unique_id']?>"><?=$film['title']?><?php if(!empty($film['alt_title'])):?>: <?=$film['alt_title']?>
+						<?php endif; ?></a></h4>
 						<p>by <?=$film['director_1']?> 
 						<?php if(!empty($film['director_2'])):?>
 							&amp; <?=$film['director_2']?>
 						<?php endif; ?><br>
 						<?=$film['color']?>, <?=$film['running_time_1']?> minutes, <?=$film['year_released']?></p>
 				</div> <!-- close individual film cell -->	
-			<hr style="width: 94%; margin-left: -2px;">
 			<?php endforeach; ?>
 			
 		</div>
