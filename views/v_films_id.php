5e3bcf86ddbe1644ba45d3b4d1fc2ce1725ede21
phantomhand <frank@phantomhand.com>
2013-12-19 17:37:25 -0500

    work on film page view

--- views/v_films_id.php
+++ views/v_films_id.php
@@ -4,15 +4,16 @@
 				
 								
 				<div> <!-- open individual film cell -->
-					<a href="/films/<?=$film['unique_id']?>"><img src="/images/<?=$film['image']?>" style="height: 330px; width: 440px; float: left; margin-right: 10px;"></a>
+					<a href="/films/<?=$film['unique_id']?>"><img class="film-lg" src="/images/<?=$film['image']?>"></a>
 						<!-- Show the film info and content -->
-						<h3><a href="/films/<?=$film['unique_id']?>"><?=$film['title']?><?php if(!empty($film['alt_title'])):?>: <?=$film['alt_title']?>
-							<?php endif; ?></a></h3>
+						<h4><?=$film['title']?><br>
+							<?php if(!empty($film['alt_title'])):?>: <?=$film['alt_title']?>
+							<?php endif; ?></h4>
 						<p>by <?=$film['director_1']?> 
 						<?php if(!empty($film['director_2'])):?>
 							&amp; <?=$film['director_2']?>
 						<?php endif; ?><br>
-						<?=$film['color']?>, <?=$film['running_time_1']?> minutes, <?=$film['year_released']?></p>
+						<?=$film['running_time_1']?> minutes, <?=$film['color']?>, <?=$film['year_released']?></p>
 				</div> <!-- close individual film cell -->	
 			<hr style="width: 94%; margin-left: -2px;">
 			
