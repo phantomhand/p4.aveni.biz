d163a4764cd75c2c21fef30ff44540f44d68f6d9
phantomhand <frank@phantomhand.com>
2013-12-19 20:30:32 -0500

    add and style image on users id page

--- views/v_users_id.php
+++ views/v_users_id.php
@@ -2,11 +2,20 @@
 	<div class="panel-body">
 		<div class="container">
 		
-		<div class="col-lg-6">
-			<h2><?=$user['first_name']?>'s Profile</h2>	
-			<p>
-			<strong>Name:</strong> <?=$user['first_name']?> <?=$user['last_name']?><br>
-
+		<div class="col-lg-8"> 
+			<h3><?=$user['first_name']?> <?=$user['last_name']?><h3>
+		</div>
+			
+		<!-- user pic -->
+		<?php if ( ($user['image'] == NULL) ): ?> 
+			<img class="avatar-lg id-profile" src="/images/avatar-blank.png" alt="User Pic" height="200" width="200"/>
+			<br>
+		
+		<?php elseif (isset ($user['image']) ): ?>
+			<img class="avatar-lg id-profile" src="<?=$user['image']?>" alt="User Pic" height="200" width="200"/>
+			<br>
+		<?php endif; ?>			
+	 	<!-- end user pic -->
 		
 		</div> <!-- /container -->
 	</div>
