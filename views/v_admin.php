cb62b07deb4ca0ed67a7aac536caab315b032d6d
phantomhand <frank@phantomhand.com>
2013-12-22 17:42:34 -0500

    add user created date

--- views/v_admin.php
+++ views/v_admin.php
@@ -6,6 +6,7 @@
 		<hr style="width: 94%; margin-left: -2px;">
 		
 		<h3><?php echo $user->first_name.'&nbsp;'.$user->last_name; ?></h3>
+		<p><strong>User since:</strong> <?=date('F j, Y', $user->created)?></p>
 		<br>
 		<h4>Actions</h4>
 		<p><a href="films/add">Add a film to the database</a></p>
