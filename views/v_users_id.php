c5b54a213aec8b4ed5c1e84181af210bd43a8782
phantomhand <frank@phantomhand.com>
2013-12-22 16:44:05 -0500

    move user image to top of profile

--- views/v_users_id.php
+++ views/v_users_id.php
@@ -1,22 +1,32 @@
 <div class="panel panel-default">
 	<div class="container">
 		
-		<div class="col-lg-8"> 
-			<h3><?=$user['first_name']?> <?=$user['last_name']?><h3>
-		
+			<h2>DER Community</h2>	
 			
-		<!-- user pic -->
-		<?php if ( ($user['image'] == NULL) ): ?> 
-			<img class="avatar-lg id-profile" src="/images/avatar-blank.png" alt="User Pic" height="200" width="200"/>
+			<hr class="hr">
+			
+			<div id="content-box">
+				<div class="col-lg-6">
+					
+					<?php if (empty($user['image']) ): ?> 
+						<img class="avatar-pf id-profile" src="/images/avatar-blank.png" alt="User Pic" height="200" width="200"/>
+						<br>
+					
+					<?php elseif (isset ($user['image']) ): ?>
+						<img class="avatar-pf" src="<?=$user['image']?>" alt="User Pic" height="200" width="200"/>
+						<br>
+					<?php endif; ?>			
+						
+					<br>
+					
+					<h3><?=$user['first_name']?> <?=$user['last_name']?></h3>
+					
+					<p>
+					<strong>User since:</strong> <?=date('F j, Y', $user['created'])?><br>
+					
+					</p>
+					
+			</div> <!-- end content box -->
 			<br>
-		
-		<?php elseif (isset ($user['image']) ): ?>
-			<img class="avatar-lg id-profile" src="<?=$user['image']?>" alt="User Pic" height="200" width="200"/>
-			<br>
-		<?php endif; ?>			
-	 	<!-- end user pic -->
-	 	</div>
-		
 	</div> <!-- /container -->
 </div>
-
