603a7fc3c3d20b1c953b7356f5f508ee522eef30
phantomhand <frank@phantomhand.com>
2013-12-22 16:32:10 -0500

    add section descriptions and link buttons to homepage

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,27 +1,27 @@
-<!-- Main jumbotron for a primary marketing message or call to action -->
+<!-- Main jumbotron -->
 <div class="jumbotron">
     <div class="container">
         <h1>Welcome</h1>
-        <p class="lead">Founded in 1968, our mission is to promote thought-provoking documentary film and media for learning about the people and cultures of the world.</p>
+        <p class="lead">Founded in 1968, our mission is to promote thought-provoking documentary<br> 
+        film and media for learning about the people and cultures of the world.</p>
 	</div> 
 </div>
 
 <div class="container">
 
-  <div class="row">
+  <div class="row home-row">
     
-	  <?php foreach($films as $film):?>
-	    <div class="col-md-4">
-	      <h3><?=$film['title']?></h3>
-	      <p>by <?=$film['director_1']?> 
-					<?php if(!empty($film['director_2'])):?>
-						&amp; <?=$film['director_2']?>
-					<?php endif; ?><br>
-					<?=$film['color']?>, <?=$film['running_time_1']?> minutes, <?=$film['year_released']?> </p>
-	      <p><a class="btn btn-primary preview-btn" role="button" href="/films/id/<?=$film['unique_id']?>">Read more &raquo;</a></p>
+	    <div class="col-md-6">
+	      <h3>Films</h3>
+	      <p>We represent films with enduring value as documents of human societies and cultures. Our films portray unique subjectivities, and offer insights into the processes underlying social and cultural continuity and change. Our collection spans diverse genres and styles, and includes seminal works in the history of ethnographic filmmaking, v&eacute;rit&eacute; and personal documentary, and other forms of experimental and non-fiction filmmaking.</p>
+	      <p><a class="btn btn-primary preview-btn" role="button" href="/films/index">Browse Films &raquo;</a></p>
 	    </div>
-	    <?php if ($films == 3) { break; }?>
-	  <?php endforeach; ?>
+	    
+	    <div class="col-md-6">
+	      <h3>Community</h3>
+	      <p>We help to build connections among a broad community of filmmakers, educators, students, archivists, organizers, and activists. Through fiscal sponsorship, workshops, and consulting, we support those working to develop media projects that document social and cultural practices, processes and beliefs. Our website serves as a place to connect with others within this community. Sign up to join the conversation.</p>
+	      <p><a class="btn btn-primary preview-btn" role="button" href="/users/signup">Join Now &raquo;</a></p>
+	    </div>
 
     </div>
 </div>
