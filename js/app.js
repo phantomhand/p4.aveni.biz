c8fddccee32bf44409346f90bd8afd19e2656bfc
phantomhand <frank@phantomhand.com>
2013-12-23 20:55:53 -0500

    improve formatting and commenting of code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-////////////////// USER PROFILE PAGE //////////////////
+////// USER PROFILE PAGE ////// 
 
-// Toggle visibility of image upload form on user profile page //
+/* Toggle visibility of image upload form on user profile page */
 $( document ).ready(function() {	    	
 	$('.upload').click(function() {
 		$('#upload').toggleClass('hidden');
@@ -8,19 +8,22 @@ $( document ).ready(function() {
 });
 
 
-////////////////// ADD A FILM FORM (ADMIN SECTION) //////////////////
+////// FORMS ////// 
 
-// Set autofocus to automatically target the first field in a form //
+/* Set autofocus to automatically target the first field in a form */
 $(document).ready(function() {
 	$('form:first *:input[type!=hidden]:first').focus();
 });
 
 
-// Error checking for film description length in admin section //
-// NOTE: this is not working Ð I think due to a conflict/quirk with Bootstrap. 
+//////  ADD A FILM FORM (ADMIN SECTION) ////// 
+
+/* Error checking for film description length in admin section */
+
+/* NOTE: this is not working Ð I think due to a conflict/quirk with Bootstrap. */ 
 $('#recipient').keyup(function() {
 
-	//Find out what's in the input field
+	/* Find out what's in the input field */
 	var recipient = $(this).val();
 	
 	var how_many_characters = recipient.length;
@@ -39,7 +42,7 @@ $('#recipient').keyup(function() {
 		$('#recipient-error').css('color', 'orange');
 		}	
 	
-	// How long was the recipient?
+	/* How long was the recipient? */
 	var length = recipient.length;
 		
 	if (length < 675) {
@@ -47,7 +50,7 @@ $('#recipient').keyup(function() {
 		$('#recipient-error').html("You have " + how_many_left + " characters left.");
 	}
 	
-	// If it was 14 characters, that's the max, so inject an error message
+	/* If it was 14 characters, that's the max, so inject an error message */
 	if (length >= 700) {
 		
 		$('#recipient-error').html("Max characters reached.");
